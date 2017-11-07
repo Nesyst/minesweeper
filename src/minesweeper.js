4b840a26bfa9b4524b26df98b1ecf19d26711488
baggio <baggio@purecobalt.com>
2017-11-07 15:44:58 -0500

    Latest changes to project 28/33 done

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -30,11 +30,52 @@ let generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     //The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
     let randomRowIndex = Math.floor(Math.random() * numberOfRows);
     let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++;
+    if(board[randomRowIndex][randomColumnIndex] !== 'B'){
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
+
   }
   return board;
 }
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighborOffsets.forEach( offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[0];
+
+    if(neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns){
+      if(bombBoard[neighborRowIndex][neighborColumnIndex] === 'B'){
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
+};
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) =>{
+  if(playerBoard[rowIndex][columnIndex] !== ' '){
+    return 'This tile has already been flipped!';
+  }else if(bombBoard[rowIndex][olumnIndex] === 'B'){
+    playerBoard[rowIndex][columnIndex] = bombBoard[rowIndex][olumnIndex];
+  }else{
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  }
+}
+
 
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
